47a4c881481419ff6512d9b767193f8ccfd7e645
Tom Meyer <tmeyer@yext.com>
2023-10-26 08:50:38 -0400

    Add support for GH Codespaces.

---
+++ .devcontainer/scripts/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source $NVM_DIR/nvm.sh
+cd /workspaces/studio-repo-demo
+nvm install 18
+nvm alias system 18
+npm install
+wget https://yext-cli-pub.s3.amazonaws.com/cli/linux/yext
+sudo mv yext /usr/local/bin
+chmod +x /usr/local/bin/yext
